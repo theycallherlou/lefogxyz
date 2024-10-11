d897715798c94269308c19860962facf066a8a91
Adam Robson <adamrayrobson@gmail.com>
2024-10-10 17:30:51 -0700

    add toggle to audio player

---
+++ app/components/AudioControls.tsx
@@ -0,0 +1,116 @@
+'use client';
+import useAudio from '@/hooks/useAudio';
+import {
+  Pause,
+  Play,
+  SkipBack,
+  SkipForward,
+  SpeakerSlash,
+  SpeakerX
+} from '@phosphor-icons/react';
+import { karla } from '@/style/fonts';
+import './AudioPlayer.css';
+
+export default function AudioControls({ isVisible }: { isVisible: boolean }) {
+  const {
+    song,
+    play,
+    volume,
+    volumeChange,
+    mute,
+    muteChange,
+    playback,
+    elapsed,
+    duration,
+    previousSong,
+    nextSong
+  } = useAudio();
+
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    volumeChange(event);
+  };
+
+  return (
+    <div className="player-container bg-text-lighter rounded-lg shadow-lg w-full max-w-md mx-auto flex flex-col justify-between items-center p-2 space-y-4 my-4">
+      <section className="w-full text-center min-h-20">
+        {playback && (
+          <article className="flex flex-col items-center justify-center p-2">
+            <div className={`${karla.variable} text-xl font-semibold`}>
+              {song.title}
+            </div>
+            <div className="text-base subpixel-antialiased">
+              {elapsed} / {duration}
+            </div>
+          </article>
+        )}
+      </section>
+
+      <section className="grid grid-cols-3 gap-4 items-center justify-center w-full min-h-20">
+        <button
+          onClick={previousSong}
+          className="control-btn flex flex-col justify-center items-center"
+        >
+          <SkipBack weight="bold" className="h-6 w-6" />
+          <span className="control-label text-xs sm:text-sm lg:text-base">
+            Previous
+          </span>
+        </button>
+
+        <button
+          onClick={play}
+          className="control-btn flex flex-col justify-center items-center"
+        >
+          {playback ? (
+            <Pause weight="bold" className="h-8 w-8 " />
+          ) : (
+            <Play weight="bold" className="h-8 w-8 " />
+          )}
+          <span className="control-label text-xs sm:text-sm lg:text-base">
+            {playback ? 'Pause' : 'Play'}
+          </span>
+        </button>
+
+        <button
+          onClick={nextSong}
+          className="control-btn flex flex-col justify-center items-center"
+        >
+          <SkipForward weight="bold" className="h-6 w-6" />
+          <span className="control-label text-xs sm:text-sm lg:text-base">
+            Next
+          </span>
+        </button>
+      </section>
+
+      <section className="flex justify-evenly items-center w-full space-x-4">
+        <button
+          onClick={muteChange}
+          className="control-btn flex flex-col justify-center items-center min-w-12"
+        >
+          {mute ? (
+            <SpeakerX weight="bold" className="h-6 w-6" />
+          ) : (
+            <SpeakerSlash weight="bold" className="h-6 w-6" />
+          )}
+          <span className="control-label text-xs sm:text-sm lg:text-base">
+            {mute ? 'Unmute' : 'Mute'}
+          </span>
+        </button>
+        <div className="relative w-full h-full flex justify-end items-center">
+          <input
+            id="volume"
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={handleVolumeChange}
+            className="volume-control"
+            style={{
+              backgroundSize: `${(volume * 100).toFixed(2)}% 100%`
+            }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
--- app/components/AudioPlayer.tsx
+++ app/components/AudioPlayer.tsx
@@ -1,116 +1,29 @@
 'use client';
-import useAudio from '@/hooks/useAudio';
-import {
-  Pause,
-  Play,
-  SkipBack,
-  SkipForward,
-  SpeakerSlash,
-  SpeakerX
-} from '@phosphor-icons/react';
-import { karla } from '@/style/fonts';
+import { useState } from 'react';
+import AudioControls from './AudioControls';
 import './AudioPlayer.css';
 
 export default function AudioPlayer() {
-  const {
-    song,
-    play,
-    volume,
-    volumeChange,
-    mute,
-    muteChange,
-    playback,
-    elapsed,
-    duration,
-    previousSong,
-    nextSong
-  } = useAudio();
+  const [isAudioPlayerVisible, setIsAudioPlayerVisible] = useState(true);
 
-  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    volumeChange(event);
+  const toggleAudioPlayer = () => {
+    setIsAudioPlayerVisible((prevState: boolean) => !prevState);
   };
 
   return (
-    <div className="player-container bg-text-lighter rounded-lg shadow-lg w-full max-w-md mx-auto flex flex-col justify-between items-center p-2 space-y-4 my-4">
-      <section className="w-full text-center min-h-20">
-        {playback && (
-          <article className="flex flex-col items-center justify-center p-2">
-            <div className={`${karla.variable} text-xl font-semibold`}>
-              {song.title}
-            </div>
-            <div className="text-base subpixel-antialiased">
-              {elapsed} / {duration}
-            </div>
-          </article>
+    <div className="player-card flex flex-col items-center p-4">
+      <button
+        onClick={toggleAudioPlayer}
+        className="toggle-button px-4 py-2 rounded transition-colors duration-300 "
+      >
+        {isAudioPlayerVisible ? 'Hide Audio Player' : 'Show Audio Player'}
+      </button>
+
+      <div className="audio-player-container mt-4 w-full">
+        {isAudioPlayerVisible && (
+          <AudioControls isVisible={isAudioPlayerVisible} />
         )}
-      </section>
-
-      <section className="grid grid-cols-3 gap-4 items-center justify-center w-full min-h-20">
-        <button
-          onClick={previousSong}
-          className="control-btn flex flex-col justify-center items-center"
-        >
-          <SkipBack weight="bold" className="h-6 w-6" />
-          <span className="control-label text-xs sm:text-sm lg:text-base">
-            Previous
-          </span>
-        </button>
-
-        <button
-          onClick={play}
-          className="control-btn flex flex-col justify-center items-center"
-        >
-          {playback ? (
-            <Pause weight="bold" className="h-8 w-8 " />
-          ) : (
-            <Play weight="bold" className="h-8 w-8 " />
-          )}
-          <span className="control-label text-xs sm:text-sm lg:text-base">
-            {playback ? 'Pause' : 'Play'}
-          </span>
-        </button>
-
-        <button
-          onClick={nextSong}
-          className="control-btn flex flex-col justify-center items-center"
-        >
-          <SkipForward weight="bold" className="h-6 w-6" />
-          <span className="control-label text-xs sm:text-sm lg:text-base">
-            Next
-          </span>
-        </button>
-      </section>
-
-      <section className="flex justify-evenly items-center w-full space-x-4">
-        <button
-          onClick={muteChange}
-          className="control-btn flex flex-col justify-center items-center min-w-12"
-        >
-          {mute ? (
-            <SpeakerX weight="bold" className="h-6 w-6" />
-          ) : (
-            <SpeakerSlash weight="bold" className="h-6 w-6" />
-          )}
-          <span className="control-label text-xs sm:text-sm lg:text-base">
-            {mute ? 'Unmute' : 'Mute'}
-          </span>
-        </button>
-        <div className="relative w-full h-full flex justify-end items-center">
-          <input
-            id="volume"
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={handleVolumeChange}
-            className="volume-control"
-            style={{
-              backgroundSize: `${(volume * 100).toFixed(2)}% 100%`
-            }}
-          />
-        </div>
-      </section>
+      </div>
     </div>
   );
 }
