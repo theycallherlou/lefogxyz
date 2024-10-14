77261818f00487f476a2bba93c3c0f784de2ceda
Adam Robson <adamrayrobson@gmail.com>
2024-10-13 18:09:25 -0700

    volume section of player styled for mobille and desktop
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- app/components/AudioControls.tsx
+++ app/components/AudioControls.tsx
@@ -4,8 +4,8 @@ import {
   Play,
   SkipBack,
   SkipForward,
-  SpeakerSlash,
-  SpeakerX
+  SpeakerNone,
+  SpeakerSlash
 } from '@phosphor-icons/react';
 import { karla } from '@/style/fonts';
 import './AudioPlayer.css';
@@ -76,42 +76,40 @@ export default function AudioControls() {
           onClick={handleNextSong}
           className="control-btn flex flex-col justify-center items-center"
         >
-          <SkipForward weight="bold" className="h-4 w-4" />
-          <label className="control-label text-xs sm:text-sm lg:text-base">
-            Next
-          </label>
+          <SkipForward weight="bold" className="" />
+          <label className="control-label">Next</label>
         </button>
       </section>
 
-      <section className="flex justify-center items-center w-full px-2">
+      <section className="w-full min-w-40 mx-auto flex justify-between items-center p-2">
         <button
+          id="mute"
+          type="button"
+          aria-label="Mute"
           onClick={handleMuteChange}
-          className="control-btn flex flex-col justify-center items-center min-w-12"
+          className="control-btn"
         >
           {mute ? (
-            <SpeakerX weight="bold" className="h-4 w-4" />
+            <SpeakerNone weight="bold" className="h-6 min-w-6 mx-auto" />
           ) : (
-            <SpeakerSlash weight="bold" className="h-4 w-4" />
+            <SpeakerSlash weight="bold" className="h-6 min-w-6 mx-auto" />
           )}
-          <label className="control-label text-xs sm:text-sm lg:text-base">
-            {mute ? 'Unmute' : 'Mute'}
-          </label>
+          <label className="control-label">{mute ? 'Unmute' : 'Mute'}</label>
         </button>
-        <div className="relative w-full h-full flex justify-end items-center">
-          <input
-            id="volume"
-            type="range"
-            min="0"
-            max="1"
-            step="0.01"
-            value={volume}
-            onChange={volumeChangeHandler}
-            className="volume-control"
-            style={{
-              backgroundSize: `${((volume ?? 0) * 100).toFixed(2)}% 100%`
-            }}
-          />
-        </div>
+
+        <input
+          id="volume"
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={volumeChangeHandler}
+          className=""
+          style={{
+            backgroundSize: `${((volume ?? 0) * 100).toFixed(2)}% 100%`
+          }}
+        />
       </section>
     </div>
   );
